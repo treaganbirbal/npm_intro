8c49b140601fbb23067083e87e3542be647af670
treaganbirbal <43793458+treaganbirbal@users.noreply.github.com>
2018-12-05 17:04:15 -0500

    Add files via upload

---
+++ npm_intro/trycalk/app.js
@@ -0,0 +1,60 @@
+const chalk = require('chalk')
+//
+// const helloBlue = (str) => {
+//   return console.log(chalk.blue(str))
+// }
+//
+// helloBlue("Hello World")
+//
+// const helloRed = (str) => {
+//   return console.log(chalk.red(str))
+// }
+//
+// helloRed("Hello World")
+//
+// const stringToColor = (str, color) => {
+//   return console.log(chalk[color](str))
+// }
+//
+// stringToColor("hello World", "red")
+
+
+// function evensBlueOddsYellow(str){
+//   let res = [];
+//   let newStr = str.split(' ');
+//   for(let i = 0; i < newStr.length; i++){
+//   if(i % 2 === 0){
+//   res.push(chalk.blue(newStr[i]))
+// } else {
+//   res.push(chalk.yellow(newStr[i]))
+// }
+// }
+// return res.join(' ')
+// }
+// console.log(evensBlueOddsYellow('this is a test'))
+
+// const angryText = (str) => {
+//   return console.log(chalk.red.underline.bold(str.toUpperCase()))
+// }
+// angryText('this is a test')
+//
+// const backgroundCyan = (str) => {
+//   return console.log(chalk.bgCyan.white(str))
+// }
+// backgroundCyan("hello World")
+
+// const boldFirstUnderlineLast = (str) => {
+//   for(i = 0; i < str.length; i++){
+//     let splitted = str.split(' ')
+//     let sliced = splitted.slice(1)
+//     // console.log(sliced)
+//     let shifted = splitted.shift()
+//     // console.log(shifted)
+//     return console.log(chalk.bold(shifted).concat(' ').concat(chalk.underline(sliced.join(' '))))
+//   }
+// }
+// boldFirstUnderlineLast("Trey is such a fucking beast")
+
+const commanLineChalk = () => {
+  
+}
